28ad94427931253b838d4ae757e299b49d48c420
LauraKabath <lara.kabath@gmail.com>
2024-03-28 22:55:16 +0100

    nove stranky
    mineraly.php, horniny.php
    odstranenie blog.php

---
+++ mineraly.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="sk">
+<?php include_once "parts/head.php" ?>
+<body>
+<?php include "parts/navigacia.php" ?>
+<main>
+    <section>
+        <div class="container">
+            <div class="card">
+                <img src="" class="card-img-top" alt="mineral">
+                <div class="card-body">
+                    <h3 class="card-title text-center">Názov</h3>
+                    <p class="card-text"><strong>Chemické zloženie: </strong></p>
+                    <p class="card-text"><strong>Chemický vzorec: </strong></p>
+                    <p class="card-text"><strong>Kryštálová sústava: </strong></p>
+                    <p class="card-text"><strong>Popis: </strong></p>
+                    <p class="card-text"><strong>Použitie: </strong></p>
+                    <p class="card-text"><strong>Výskyt: </strong></p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php include "parts/footer.php"?>
+</body>
+</html>
--- parts/navigacia.php
+++ parts/navigacia.php
@@ -15,15 +15,8 @@
                         <div class="navbar-nav mx-auto">
                             <a class="nav-link text-white" href="index.php"><i class="fa fa-home"></i> Domov</a>
                             <a class="nav-link text-white" href="o_nas.php">O nás</a>
-                            <div class="dropdown">
-                                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown">Blog</a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="blog.php">Sysliareň</a>
-                                    <a class="dropdown-item" href="blog_1.php">Objavujeme tajomstvá hradu Muráň</a>
-                                    <a class="dropdown-item" href="blog_2.php">Sysle na Muránskej planine</a>
-                                    <a class="dropdown-item" href="blog_3.php">Sysle ako tajomné stvorenia prírody</a>
-                                </div>
-                            </div>
+                            <a class="nav-link text-white" href="mineraly.php">Minerály</a>
+                            <a class="nav-link text-white" href="horniny.php">Horniny</a>
                             <a class="nav-link text-white" href="galeria.php">Galéria</a>
                             <a class="nav-link text-white" href="kontakt.php">Kontakt</a>
                         </div>
