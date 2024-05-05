336722184748a7542425cc24f4b3ead533a4eb4f
LauraKabath <lara.kabath@gmail.com>
2024-05-05 22:31:57 +0200

    Update User.php
    password change

--- classes/User.php
+++ classes/User.php
@@ -39,8 +39,52 @@ private $conn;
         }
     }
 
-    public function changePassword(){
-        return true;
+    public function changePassword($id, $oldPassword, $newPassword, $repeatedNew){
+        if ($newPassword != $repeatedNew){
+            echo "Hesla sa nezhoduju";
+            return false;
+        }
+
+        if (!$this->verifyOldPassword($id, $oldPassword)){
+            echo "Nespravne zadane povodne heslo";
+            return false;
+        }
+
+        $hashedNew = password_hash($newPassword, PASSWORD_BCRYPT);
+
+        $query = "UPDATE tabuzivatel SET heslo = :newPassword WHERE uzi_ID = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':newPassword', $hashedNew);
+
+        try {
+            $change = $stmt->execute();
+            return $change;
+        } catch (\Exception $exception){
+            echo "Chyba pri zmene hesla".$exception->getMessage();
+            $this->conn->rollback();
+        }
+
+    }
+
+    private function verifyOldPassword($id, $oldPassword){
+        $query = "SELECT * FROM tabuzivatel WHERE uzi_ID = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+
+        try {
+            $stmt->execute();
+            $userdata = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if (password_verify($oldPassword, $userdata['heslo'])){
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\Exception $exception){
+            echo "Chyba pri overovani stareho hesla".$exception->getMessage();
+            $this->conn->rollback();
+        }
     }
 
 }
